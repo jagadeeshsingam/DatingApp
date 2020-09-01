78981bfd07b43596d9eeb05ba91cd7ab38212c76
jagadeesh467 <69083349+jagadeesh467@users.noreply.github.com>
2020-09-01 23:03:24 +0530

    module 5 miss

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -41,7 +41,6 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            throw new Exception("hi, expception here.....");
             var userFromRepo = await _repo.Login(userForLoginDto.UserName.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
